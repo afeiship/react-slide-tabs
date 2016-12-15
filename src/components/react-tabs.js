6dec767048ad41a7b85a4ca46b0963a8abab4cfc
feizheng <1290657123@qq.com>
2016-12-15 14:01:25 +0800

    basic tabs

--- src/components/react-tabs.js
+++ src/components/react-tabs.js
@@ -3,21 +3,49 @@ import classNames from 'classnames';
 
 export default class extends React.Component{
   static propTypes = {
-    cssClass:React.PropTypes.string
+    cssClass:React.PropTypes.string,
+    activeIndex:React.PropTypes.number,
   };
 
   static defaultProps = {
+    activeIndex:0
   };
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      activeIndex:props.activeIndex
+    };
   }
 
   render(){
     return (
       <div className={classNames('react-tabs',this.props.cssClass)}>
-        Hello React!
+        <ul className="react-tabs-menu">
+          {React.Children.map(this.props.children, (element, index) => {
+            return (<li key={index} className={classNames('react-tabs-menu-item',{'tab-active':this.state.activeIndex == index})} onClick={() => {this.setState({activeIndex: index})}}>{element.props.menu}</li>)
+          })}
+        </ul>
+        
+        <div className="react-tabs-content">
+          <div className="react-tabs-content-scroller" style={{
+              width:`${this.props.children.length*100}%`
+            }}>
+            {React.Children.map(this.props.children, (element, index) => {
+              return (
+                <div
+                  key={index}
+                  style={{
+                     width:`${100/this.props.children.length}%`,
+                     transform:`translateX(-${100*(this.state.activeIndex)}%)`
+                  }}
+                 className={classNames('react-tabs-content-item',{'tab-active':this.state.activeIndex == index})}>
+                 {element.props.children}
+               </div>
+              )
+            })}
+          </div>
+        </div>
       </div>
     );
   }
--- src/dev.js
+++ src/dev.js
@@ -6,7 +6,17 @@ class App extends React.Component{
   render(){
     return (
       <div className="hello-react-tabs">
-        <ReactTabs />
+        <ReactTabs>
+          <div cssClass="bd" menu="red">
+            Read tab contet...
+          </div>
+          <div cssClass="bd" menu="blue">
+            Blue tab contet...
+          </div>
+          <div cssClass="bd" menu="yellow">
+            Yellow tab contet...
+          </div>
+        </ReactTabs>
     </div>
     );
   }
