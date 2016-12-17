4a69357f0140a020cafbf741cd26e0c45ffd60a7
feizheng <1290657123@qq.com>
2016-12-17 20:22:02 +0800

    optimize code

--- src/components/react-tabs.js
+++ src/components/react-tabs.js
@@ -1,72 +1,18 @@
 import './style.scss';
 import classNames from 'classnames';
 import Swipeable from 'react-swipeable';
-import Measure from 'react-measure';
+import {ReactSwipeViews} from 'react-swipe-views';
 
-
-export default class extends React.Component{
-  static propTypes = {
-    cssClass:React.PropTypes.string,
-    activeIndex:React.PropTypes.number,
-    duration:React.PropTypes.number,
-  };
-
-  static defaultProps = {
-    activeIndex:0,
-    duration:0.3
-  };
+export default class extends ReactSwipeViews{
 
   constructor(props) {
     super(props);
-    this.state = {
-      activeIndex:props.activeIndex,
-      dimensions:{},
-      translateX:0,
-      duration:props.duration
+
+    this._length = props.children.length;
+    this._boundary = {
+      min: 0,
+      max: this._length - 1
     };
-  }
-
-  _onSwipedLeft(ev){
-    var index = this.state.activeIndex + 1;
-    var length = this.props.children.length;
-    if(index==length){
-      return this.toIndex(this.state.activeIndex);
-    }
-    this.toIndex(index);
-  }
-
-  _onSwipedRight(ev){
-    var index = this.state.activeIndex - 1;
-    if(index<0){
-      return this.toIndex(this.state.activeIndex);
-    }
-    this.toIndex(index);
-  }
-
-  toIndex(inIndex){
-    this.setState({
-      activeIndex:inIndex,
-      duration:this.props.duration,
-      translateX:`-${inIndex * 100/ this.props.children.length}%`
-    });
-  }
-
-
-  _onSwipingLeft(ev,deltaX){
-    var _translateX = this.state.activeIndex * this.state.dimensions.width;
-    this.setState({
-      duration:0,
-      translateX:`-${_translateX+deltaX}px`
-    });
-
-  }
-
-  _onSwipingRight(ev,deltaX){
-    var _translateX = this.state.activeIndex * this.state.dimensions.width;
-    this.setState({
-      duration:0,
-      translateX: `${-_translateX+deltaX}px`
-    });
   }
 
   render(){
@@ -76,40 +22,42 @@ export default class extends React.Component{
           <ul className="react-tabs-menu">
             {React.Children.map(this.props.children, (element, index) => {
               return (<li
-                style={{width:`${100/this.props.children.length}%`}}
+                style={{width:`${100/this._length}%`}}
                 key={index}
                 className={classNames('react-tabs-menu-item',{'tab-active':this.state.activeIndex == index})}
-                onClick={() => {this.toIndex(index);}}>{element.props.menu}</li>)
+                onClick={() => {this.play(index);}}>{element.props.menu}</li>)
             })}
           </ul>
           <div
             style={{
-              width:`${100/this.props.children.length}%`,
-              left:`${100*this.state.activeIndex/this.props.children.length}%`
+              width:`${100/this._length}%`,
+              left:`${100*this.state.activeIndex/this._length}%`
             }} className="bar"></div>
         </div>
-        <Measure onMeasure={(dimensions) => {
-          this.setState({dimensions})
-        }}>
         <Swipeable
           className="bd"
-          onSwipedLeft={this._onSwipedLeft.bind(this)}
-          onSwipedRight={this._onSwipedRight.bind(this)}
-          onSwipingLeft={this._onSwipingLeft.bind(this)}
-          onSwipingRight={this._onSwipingRight.bind(this)}
+          onSwipingLeft={this.onSwipingNext.bind(this)}
+          onSwipingRight={this.onSwipingPrev.bind(this)}
+          onSwipedLeft={this.next.bind(this)}
+          onSwipedRight={this.prev.bind(this)}
+          style={{
+            width:`${this.state.bound.width}px`
+          }}
           >
-          <div className="react-tabs-content">
+          <div className="react-tabs-content" ref="root">
             <div className="react-tabs-content-scroller" style={{
-                width:`${this.props.children.length*100}%`,
-                transition:`transform ${this.state.duration}s`,
-                transform:`translateX(${this.state.translateX})`
-              }}>
+                  width:`${this._length*100}%`,
+                  transition:`transform ${this.state.duration}s ${this.state.animate}`,
+                  WebkitTransition:`transform ${this.state.duration}s ${this.state.animate}`,
+                  transform:`translate(${this.state.translate},0)`,
+                  WebkitTransform:`translate(${this.state.translate},0)`
+                }}>
               {React.Children.map(this.props.children, (element, index) => {
                 return (
                   <div
                     key={index}
                     style={{
-                      width:`${100/this.props.children.length}%`
+                      width:`${100/this._length}%`
                     }}
                    className={classNames('react-tabs-content-item',{'tab-active':this.state.activeIndex == index})}>
                    {element.props.children}
@@ -119,7 +67,6 @@ export default class extends React.Component{
             </div>
           </div>
         </Swipeable>
-        </Measure>
       </div>
     );
   }
